92ed686097d540daee475733a878bcbf0a459ff8
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2025-10-02 23:51:02 +0530

    -

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  root: './frontend', 
   plugins: [react(), tailwindcss()],
 });
